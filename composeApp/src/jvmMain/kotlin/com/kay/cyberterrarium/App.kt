816945ac69d9be2e0fb73712b8d9feca53728d50
Kayxue <s0990006@gmail.com>
2026-05-30 21:30:10 +0800

    Remove unused import

--- composeApp/src/jvmMain/kotlin/com/kay/cyberterrarium/App.kt
+++ composeApp/src/jvmMain/kotlin/com/kay/cyberterrarium/App.kt
@@ -4,11 +4,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.automirrored.outlined.List
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.*
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.QueryStats
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.QueryStats
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import page.Home
--- composeApp/src/jvmMain/kotlin/terrarium/Water.kt
+++ composeApp/src/jvmMain/kotlin/terrarium/Water.kt
@@ -2,14 +2,12 @@ package terrarium
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCanvas() {
